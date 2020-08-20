17d7958d1436a1b5e3fe9db477a57c865d4cf477
Martin Sander-Thomsen <joghur@gmail.com>
2020-08-20 12:33:44 +0200

    readme file and some configs

---
+++ App.js
@@ -0,0 +1,62 @@
+/* eslint-disable global-require */
+import React, { useState } from 'react';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+import ReduxThunk from 'redux-thunk';
+import { YellowBox } from 'react-native';
+
+import wishesReducer from './store/reducers/wishesReducer';
+import authReducer from './store/reducers/authReducer';
+import usersReducer from './store/reducers/usersReducer';
+import NavigationContainer from './navigation/NavigationContainer';
+
+const rootReducer = combineReducers({
+  wishes: wishesReducer,
+  users: usersReducer,
+  auth: authReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'chilanka-regular': require('./assets/fonts/Chilanka-Regular.ttf'),
+    comingSoon: require('./assets/fonts/ComingSoon.ttf'),
+    cutivemono: require('./assets/fonts/CutiveMono.ttf'),
+    'dancingScript-bold': require('./assets/fonts/DancingScript-Bold.ttf'),
+    'dancingScript-regular': require('./assets/fonts/DancingScript-Regular.ttf'),
+    purisa: require('./assets/fonts/Purisa.ttf'),
+    'purisa-bold': require('./assets/fonts/Purisa-Bold.ttf'),
+    'purisa-bold-oblique': require('./assets/fonts/Purisa-BoldOblique.ttf'),
+    'purisa-oblique': require('./assets/fonts/Purisa-Oblique.ttf'),
+    sawasdee: require('./assets/fonts/Sawasdee.ttf'),
+    'sawasdee-bold': require('./assets/fonts/Sawasdee-Bold.ttf'),
+    'sawasdee-bold-oblique': require('./assets/fonts/Sawasdee-BoldOblique.ttf'),
+    'sawasdee-oblique': require('./assets/fonts/Sawasdee-Oblique.ttf'),
+  });
+};
+
+export default function App() {
+  // ignoring for now the yellow warning about too long setTimeout (auto logout)
+  YellowBox.ignoreWarnings(['Setting a timer']);
+
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setFontLoaded(true);
+        }}
+      />
+    );
+  }
+  return (
+    <Provider store={store}>
+      <NavigationContainer />
+    </Provider>
+  );
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-GaveGuide-RN
+# GaveGuiden-RN
+
+### Initial files for gift wishes projekt in react native
+ * App language: danish at the moment  
+ * Expo: sdk 38  
+ * react-navigation: v3  
+
+**settings.json** file must be created in root folder. It contains database options (firebase API and key):  
+    {  
+  "API_Key": "KEY",  
+  "DB": "YOUR-FIREBASE-URL/"  
+}
